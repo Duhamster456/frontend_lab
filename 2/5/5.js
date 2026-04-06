59e564fe83d6b33f7f371bda17e0ce0d64ab0702
Duhamster <ergerg>
2026-04-06 04:36:00 +0300

    half of lab3

--- 2/5/5.js
+++ 2/5/5.js
@@ -5,7 +5,7 @@ const output = document.getElementById("output");
 let addrArr = [];
 let goodFlag = true;
 
-for(let i = 0; i < addrCount; i++){
+for(let i = 0; i < 0; i++){
     addrArr[i] = prompt("Введите IP-адрес:", "8.8.8.8");
 }
 
@@ -26,4 +26,19 @@ async function checkCountries() {
     }
 }
 
-checkCountries();
+function checkCountry2(address){
+	let russiaPromise = fetch(`https://json.geoiplookup.io/${address}`);
+	russiaPromise.then((value) => {
+		return value.json();
+	}).then((value) => {
+		console.log(value);
+		if(value["country_code"] === "RU"){
+			output.textContent = "Our services are not available in your country";
+		}
+		else {
+			output.textContent = "Welcome to our website";
+		}
+	})
+}
+
+checkCountry2("8.8.8.8");
--- 2/OOP/OOP.js
+++ 2/OOP/OOP.js
@@ -1,76 +1,74 @@
 class User {
-  #age;
-  #tel;
+	#age;
+	#tel;
 
-  constructor(name, age, tel) {
-    this.name = name;
-    this.age = age;
-    this.tel = tel;
-  }
+	constructor(name, age, tel) {
+		this.name = name;
+		this.age = age;
+		this.tel = tel;
+	}
 
-  get age() {
-    return this.#age;
-  }
+	get age() {
+		return this.#age;
+	}
 
-  set age(value) {
-    if (Number.isInteger(value) && value >= 1 && value <= 100) {
-      this.#age = value;
-    } else {
-      throw new Error('Age must be integer between 1 and 100');
-    }
-  }
+	set age(value) {
+		if (Number.isInteger(value) && value >= 1 && value <= 100) {
+			this.#age = value;
+		} else {
+			throw new Error('Age must be integer between 1 and 100');
+		}
+	}
 
-  get tel() {
-    return this.#tel;
-  }
+	get tel() {
+		return this.#tel;
+	}
 
-  set tel(value) {
-    const phoneRegex = /^\+7\d{10}$/;
-    if (phoneRegex.test(value)) {
-      this.#tel = value;
-    } else {
-      throw new Error('Invalid phone format. Use +7xxxxxxxxxx');
-    }
-  }
+	set tel(value) {
+		const phoneRegex = /^\+7\d{10}$/;
+		if (phoneRegex.test(value)) {
+			this.#tel = value;
+		} else {
+			throw new Error('Invalid phone format. Use +7xxxxxxxxxx');
+		}
+	}
 
-  hello() {
-    const msg = `Hi! My name is ${this.name}. And I am ${this.age} years old.`;
-    console.log(msg);
-    alert(msg);
-  }
+	hello() {
+		const msg = `Hi! My name is ${this.name}. And I am ${this.age} years old.`;
+		console.log(msg);
+	}
 }
 
 function UserFunc(name, age) {
-  this.name = name;
-  this.age = age;
+	this.name = name;
+	this.age = age;
 }
+
 UserFunc.prototype.hello = function() {
-  const msg = `Hi! My name is ${this.name}. And I am ${this.age} years old.`;
-  console.log(msg);
-  alert(msg);
+	const msg = `Hi! My name is ${this.name}. And I am ${this.age} years old.`;
+	console.log(msg);
 };
 
 class Student extends User {
-  #knowledge = 0;
+	#knowledge = 0;
 
-  get knowledge() {
-    return this.#knowledge;
-  }
+	get knowledge() {
+		return this.#knowledge;
+	}
 
-  learn() {
-    this.#knowledge++;
-  }
+	learn() {
+		this.#knowledge++;
+	}
 
-  hello() {
-    const msg = `Hi! My name is ${this.name}. I am ${this.age} years old. And I am a student!`;
-    console.log(msg);
-    alert(msg);
-  }
+	hello() {
+		const msg = `Hi! My name is ${this.name}. I am ${this.age} years old. And I am a student!`;
+		console.log(msg);
+	}
 }
 
 Array.prototype.reverse = function() {
-  const copy = [...this];
-  return copy.concat(copy);
+	const copy = [...this];
+	return copy.concat(copy);
 };
 
 const user1 = new User('Alice', 25, '+71234567890');
@@ -87,8 +85,7 @@ student.learn();
 student.learn();
 console.log('Student knowledge:', student.knowledge);
 
-const arr = [1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
 const reversedArr = arr.reverse();
 console.log('Original array:', arr);
 console.log('Reversed (duplicated):', reversedArr);
-alert('Check console for results');
