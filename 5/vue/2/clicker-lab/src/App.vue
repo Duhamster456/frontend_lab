dde228bd87533bf1a0faf27f27e99959da018491
Duhamster <ergerg>
2026-05-09 02:15:30 +0300

    vue intro added

---
+++ 5/vue/2/clicker-lab/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Clicker from './components/Clicker.vue'
+
+function handleButtonClick(clickerNumber) {
+	alert(`Clicker ${clickerNumber} clicked`)
+}
+
+</script>
+<template>
+	<div class="clickers-container">
+		<Clicker
+			v-for="n in 100"
+			:key="n"
+			:amount=1
+			@button-clicked="handleButtonClick(n)"
+		>
+			<button class="my-custom-btn">➕ Добавить</button>
+		</Clicker>
+	</div>
+</template>
+<style scoped>
+.clickers-container {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 10px;
+}
+</style>
---
+++ 5/vue/2/clicker-lab/src/components/Clicker.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  amount: Number
+})
+
+const emit = defineEmits(['button-clicked'])
+
+const count = ref(0);
+
+function onClickFunc(){
+	count.value += props.amount;
+	emit('button-clicked');
+}
+
+</script>
+
+<template>
+	<div class="clicker">
+		<span class="count">Count: {{ count }}</span>
+		<div class="clicker-button-wrapper" @click="onClickFunc">
+		<slot>Click me</slot>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.clicker {
+	background-color: #BBAAFF;
+}
+</style>
